c4d34cb2f8be6e45262ded57d93a4108471d3d88
Alvin <alvinhtml@gmail.com>
2022-12-28 17:10:16 +0800

    changed configs

--- src/home.js
+++ src/home.js
@@ -1,42 +1,59 @@
-import React, {Component, useState, useEffect} from 'react';
-import ReactDOM, {render} from 'react-dom';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import React, { Component, useState, useEffect } from 'react'
+import ReactDOM, { render } from 'react-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 // import {ActiveModal} from 'react-miniui';
-import {
-  Miniui
-} from 'react-miniui';
-import 'react-miniui/dist/miniui.css';
+import { Miniui } from 'react-miniui'
+import 'react-miniui/dist/miniui.css'
 // import '../../react-miniui/dist/miniui.css';
 
-import './scss/page.scss';
+import './scss/page.scss'
 
-import ButtonPage from './container/button.js';
-import FormPage from './container/form.js';
-import ColumnPage from './container/column.js';
-import IconPage from './container/icon.js';
-import TextPage from './container/text.js';
-import UsePage from './container/use.js';
-import BadgePage from './container/badge.js';
-import DropdownPage from './container/dropdown.js';
-import OverlayPage from './container/overlay.js';
-import ModalPage from './container/modal.js';
-import SheetPage from './container/sheet.js';
-import TabsPage from './container/tabs.js';
+import ButtonPage from './container/button.js'
+import FormPage from './container/form.js'
+import ColumnPage from './container/column.js'
+import IconPage from './container/icon.js'
+import TextPage from './container/text.js'
+import UsePage from './container/use.js'
+import BadgePage from './container/badge.js'
+import DropdownPage from './container/dropdown.js'
+import OverlayPage from './container/overlay.js'
+import ModalPage from './container/modal.js'
+import SheetPage from './container/sheet.js'
+import TabsPage from './container/tabs.js'
 
 class Header extends Component {
   render() {
-    return(
+    return (
       <header className="mui header flex">
         <div className="flex-control-label">
-          <h1 className="logo"><span className="color-red">M</span><span className="color-green">ini</span><span className="color-teal">UI</span></h1>
+          <h1 className="logo">
+            <span className="color-red">M</span>
+            <span className="color-green">ini</span>
+            <span className="color-teal">UI</span>
+          </h1>
         </div>
         <div className="flex-controls">
           <div className="links">
-            <a href="https://www.npmjs.com/package/react-miniui" target="_blank">
-              <svg height="16" viewBox="0 0 780 250"><path fill="#231F20" d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"></path></svg>
+            <a
+              href="https://www.npmjs.com/package/react-miniui"
+              target="_blank"
+            >
+              <svg height="16" viewBox="0 0 780 250">
+                <path
+                  fill="#231F20"
+                  d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"
+                ></path>
+              </svg>
             </a>
-            <iframe src="https://ghbtns.com/github-btn.html?user=alvinhtml&repo=react-miniui&type=watch&count=false" allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=alvinhtml&repo=react-miniui&type=watch&count=false"
+              allowtransparency="true"
+              frameborder="0"
+              scrolling="0"
+              width="62"
+              height="20"
+            ></iframe>
           </div>
         </div>
       </header>
@@ -44,49 +61,108 @@ class Header extends Component {
   }
 }
 
-
 class SlideBar extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      fixed: false
+      fixed: false,
     }
   }
 
   render() {
-    return(
+    return (
       <div className={`miniui-summary ${this.state.fixed ? 'fixed' : ''}`}>
         <ul>
           <li className="header">用法</li>
           <li>
             <ul>
-              <li><Link to="/use"><span>Use</span><span className="comment">安装和使用</span></Link></li>
+              <li>
+                <Link to="/use">
+                  <span>Use</span>
+                  <span className="comment">安装和使用</span>
+                </Link>
+              </li>
             </ul>
           </li>
           <li className="header">基础</li>
           <li>
             <ul>
-              <li><Link to="/text"><span>Text</span><span className="comment">文字</span></Link></li>
-              <li><Link to="/icon"><span>Icon</span><span className="comment">图标</span></Link></li>
-              <li><Link to="/badge"><span>Badge</span><span className="comment">徽章</span></Link></li>
-              <li><Link to="/button"><span>Button</span><span className="comment">按钮</span></Link></li>
-              <li><Link to="/input"><span>Input</span><span className="comment">输入框</span></Link></li>
+              <li>
+                <Link to="/text">
+                  <span>Text</span>
+                  <span className="comment">文字</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/icon">
+                  <span>Icon</span>
+                  <span className="comment">图标</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/badge">
+                  <span>Badge</span>
+                  <span className="comment">徽章</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/button">
+                  <span>Button</span>
+                  <span className="comment">按钮</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/input">
+                  <span>Input</span>
+                  <span className="comment">输入框</span>
+                </Link>
+              </li>
             </ul>
           </li>
           <li className="header">布局</li>
           <li>
             <ul>
-              <li><Link to="/column"><span>Grid</span><span className="comment">栅格</span></Link></li>
+              <li>
+                <Link to="/column">
+                  <span>Grid</span>
+                  <span className="comment">栅格</span>
+                </Link>
+              </li>
             </ul>
           </li>
           <li className="header">功能</li>
           <li>
             <ul>
-              <li><Link to="/tabs"><span>Tabs</span><span className="comment">选项卡</span></Link></li>
-              <li><Link to="/dropdown"><span>Dropdown</span><span className="comment">下载菜单</span></Link></li>
-              <li><Link to="/overlay"><span>Overlay</span><span className="comment">弹出浮层</span></Link></li>
-              <li><Link to="/modal"><span>Modal</span><span className="comment">弹出窗口</span></Link></li>
-              <li><Link to="/sheet"><span>Sheet</span><span className="comment">弹出面板</span></Link></li>
+              <li>
+                <Link to="/tabs">
+                  <span>Tabs</span>
+                  <span className="comment">选项卡</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/dropdown">
+                  <span>Dropdown</span>
+                  <span className="comment">下载菜单</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/overlay">
+                  <span>Overlay</span>
+                  <span className="comment">弹出浮层</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/modal">
+                  <span>Modal</span>
+                  <span className="comment">弹出窗口</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/sheet">
+                  <span>Sheet</span>
+                  <span className="comment">弹出面板</span>
+                </Link>
+              </li>
             </ul>
           </li>
         </ul>
@@ -96,41 +172,36 @@ class SlideBar extends Component {
 }
 
 class Home extends Component {
-  constructor(){
-    super();
-    this.mref = React.createRef();
+  constructor() {
+    super()
+    this.mref = React.createRef()
   }
   componentDidMount() {
     // console.log("this.mref", this.mref.current);
   }
   render() {
-    return (
-      <div>
-        home
-      </div>
-    )
+    return <div>home</div>
   }
 }
 
-
 class Main extends Component {
   render() {
-    return(
+    return (
       <div className="miniui-body">
         <Switch>
-          <Route exact path='/' component={UsePage}/>
-          <Route path='/use' component={UsePage}/>
-          <Route path='/text' component={TextPage}/>
-          <Route path='/button' component={ButtonPage}/>
-          <Route path='/input' component={FormPage}/>
-          <Route path='/column' component={ColumnPage}/>
-          <Route path='/icon' component={IconPage}/>
-          <Route path='/badge' component={BadgePage}/>
-          <Route path='/dropdown' component={DropdownPage}/>
-          <Route path='/overlay' component={OverlayPage}/>
-          <Route path='/modal' component={ModalPage}/>
-          <Route path='/sheet' component={SheetPage}/>
-          <Route path='/tabs' component={TabsPage}/>
+          <Route exact path="/" component={UsePage} />
+          <Route path="/use" component={UsePage} />
+          <Route path="/text" component={TextPage} />
+          <Route path="/button" component={ButtonPage} />
+          <Route path="/input" component={FormPage} />
+          <Route path="/column" component={ColumnPage} />
+          <Route path="/icon" component={IconPage} />
+          <Route path="/badge" component={BadgePage} />
+          <Route path="/dropdown" component={DropdownPage} />
+          <Route path="/overlay" component={OverlayPage} />
+          <Route path="/modal" component={ModalPage} />
+          <Route path="/sheet" component={SheetPage} />
+          <Route path="/tabs" component={TabsPage} />
         </Switch>
       </div>
     )
@@ -138,10 +209,18 @@ class Main extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = 1
+  }
+  handleClcik() {
+    this.state++
+  }
   render() {
-    return(
+    return (
       <main className="main flex">
         <div className="flex-control-label">
+          <button onClick={this.handleClcik()}>test {this.state}</button>
           <SlideBar />
         </div>
         <div className="flex-controls">
@@ -160,4 +239,5 @@ render(
       <Miniui />
     </div>
   </BrowserRouter>,
-document.getElementById('webApplication'))
+  document.getElementById('webApplication')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -6,147 +6,149 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') //抽离 css 文件，使用这个插件需要单独配置JS和CSS压缩
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin') //压缩JS
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin') //压缩CSS
-const FileManagerPlugin = require('filemanager-webpack-plugin'); //webpack copy move delete mkdir archive
+const FileManagerPlugin = require('filemanager-webpack-plugin') //webpack copy move delete mkdir archive
 
-console.log(path.resolve(__dirname, '~/'));
+console.log(path.resolve(__dirname, '~/'))
 
 module.exports = {
-
-    optimization: { //优化项
-        minimizer: [ //压缩优化
-            new UglifyJsPlugin({
-                cache: true, //缓存
-                parallel: true, //并发打包
-                sourceMap: true // set to true if you want JS source maps
-            }),
-            new OptimizeCSSAssetsPlugin()
-        ]
-    },
-
-    mode: 'development', //两种模式， production (生产模式) development（开发模式）
-
-    entry: {
-        index: './src/home.js'
-    },
-
-    devtool: 'source-map', //源码映射，生成一个映射文件，帮我们定位源码文件
-
-    output: {
-        filename: '[name].js', //打包后的文件名
-        path: path.resolve(__dirname, './dist') //路径必须是绝对路径
-    },
-
-    resolve: {
-        modules: [path.resolve('node_modules')],
-        alias: {
-          '~': path.resolve(__dirname, './src')
-        },
-        extensions: ['.js', '.jsx', '.scss', '.less', '.css'] //配置省略后缀名
-    },
-
-    module: { //模块
-
-        rules: [ //规则
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader'
-                ]
-            },
-            {
-                test: /\.(scss|sass)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    {
-                      loader: 'sass-loader',
-                      options: {
-                        includePaths: ['./node_modules/normalize-scss/sass']
-                      }
-                    }
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/, //排除
-                use: {
-                    loader: 'babel-loader',
-                    options: { //用 babel-loader 转化 ES6-ES5
-                        presets: [ //这里是大插件集合
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                            '@babel/preset-flow'
-                        ],
-                        plugins: [ //这里可以配置一些小的插件
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-syntax-dynamic-import'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.(jpg|png|gif|jpeg|bmp|eot|svg|ttf|woff|woff2)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 50 * 1024,
-                        outputPath: './',
-                    }
-                }
-            }
-        ]
-    },
-
-
-    plugins: [ //数组，放着所有 webpack 插件
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            // minify: {
-            //     removeComments: true,
-            //     removeAttributeQuotes: true,
-            //     collapseWhitespace: true
-            // },
-            hash: true,
-            chunks: ['index']
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].min.css'
-        }),
-
-        new FileManagerPlugin({
-            onEnd: {
-                copy: [{
-                    source: path.resolve(__dirname, 'favicon.ico'),
-                    destination: path.resolve(__dirname, 'dist/')
-                }]
-            }
-        }),
-
-        new webpack.DefinePlugin({
-            DEV: JSON.stringify('dev'),
-        }),
-
+  optimization: {
+    //优化项
+    minimizer: [
+      //压缩优化
+      new UglifyJsPlugin({
+        cache: true, //缓存
+        parallel: true, //并发打包
+        sourceMap: true, // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin(),
     ],
+  },
 
+  mode: 'development', //两种模式， production (生产模式) development（开发模式）
 
-    // watch: true,
-    // watchOptions: {
-    //     poll: 2000, //每秒问我多少次
-    //     aggregateTimeout: 2000, //防抖
-    //     ignored: /node_modules|vendor|build|public|resources/
-    // },
+  entry: {
+    index: './src/home.js',
+  },
 
-    devServer: {
-        // host: '0.0.0.0',
-        port: 8080,
-        progress: true, //进度条
-        contentBase: './dist', //配置目录
-        open: true, //在DevServer第一次构建完成时，自动用浏览器打开网页
-        historyApiFallback: true
-    }
+  devtool: 'source-map', //源码映射，生成一个映射文件，帮我们定位源码文件
+
+  output: {
+    filename: '[name].js', //打包后的文件名
+    path: path.resolve(__dirname, './dist'), //路径必须是绝对路径
+  },
+
+  resolve: {
+    modules: [path.resolve('node_modules')],
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.scss', '.less', '.css'], //配置省略后缀名
+  },
+
+  module: {
+    //模块
+
+    rules: [
+      //规则
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules/normalize-scss/sass'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, //排除
+        use: {
+          loader: 'babel-loader',
+          options: {
+            //用 babel-loader 转化 ES6-ES5
+            presets: [
+              //这里是大插件集合
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-flow',
+            ],
+            plugins: [
+              //这里可以配置一些小的插件
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|bmp|eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 50 * 1024,
+            outputPath: './',
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    //数组，放着所有 webpack 插件
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      // minify: {
+      //     removeComments: true,
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true
+      // },
+      hash: true,
+      chunks: ['index'],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].min.css',
+    }),
+
+    new FileManagerPlugin({
+      onEnd: {
+        copy: [
+          {
+            source: path.resolve(__dirname, 'favicon.ico'),
+            destination: path.resolve(__dirname, 'dist/'),
+          },
+        ],
+      },
+    }),
+
+    new webpack.DefinePlugin({
+      DEV: JSON.stringify('dev'),
+    }),
+  ],
+
+  // watch: true,
+  // watchOptions: {
+  //     poll: 2000, //每秒问我多少次
+  //     aggregateTimeout: 2000, //防抖
+  //     ignored: /node_modules|vendor|build|public|resources/
+  // },
+
+  devServer: {
+    // host: '0.0.0.0',
+    port: 8086,
+    progress: true, //进度条
+    contentBase: './dist', //配置目录
+    open: true, //在DevServer第一次构建完成时，自动用浏览器打开网页
+    historyApiFallback: true,
+  },
 }
